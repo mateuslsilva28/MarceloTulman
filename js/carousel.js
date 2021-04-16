60f4bfc92842baf7d085e50ce0954a6d33b815e4
Mateus <mateuslsilva28@gmail.com>
2021-04-15 22:41:08 -0300

    C4 - Carousel

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", ()=>{
+    var carousel = document.getElementsByClassName("banner")[0]
+    var left = document.getElementsByClassName("fa-arrow-left")[0]
+    var right = document.getElementsByClassName("fa-arrow-right")[0]
+    var qtdImg = 2;
+    var cont = 1    
+    right.addEventListener("click", ()=>{
+        cont++
+        if(cont > qtdImg)cont=1
+        carousel.style=`background-image: url("../img/slide-${cont}.webp")`
+    })
+    left.addEventListener("click", ()=>{
+        cont--
+        if(cont < 1)cont = 2
+        carousel.style=`background-image: url("../img/slide-${cont}.webp")`
+    })
+})
